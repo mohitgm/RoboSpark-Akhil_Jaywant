3cb78d79413d6c6cb009f16f919b8b2f6d761f06
Mohit G Marathe <mohit.marathe19@vit.edu>
2020-07-05 16:34:16 +0530

    oops

---
+++ 04-07-20_DS_TASK1_Mohit_Marathe/tk1.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+ class student{
+     public:
+     string name;
+     int roll;
+     int *marks=new int[5];
+     
+     int total=0;
+     double cgpa;
+ student(){
+     name="default";
+     roll=0;
+     int marks[5]={0,0,0,0,0};
+     total=0;
+     cgpa=0;}
+ void stud_inp(){
+     cout<<"enter details "<<endl;
+     cout<<"name: ";
+     cin>>name;
+     cout<<"roll ";
+     cin>>roll;
+     for(int i=0;i<5;i++){
+         cin>>marks[i];
+     }}
+ int tot(int *arr){
+     for(int i=0;i<5;i++){
+         total=total+arr[i];
+     }
+     return total;
+ }
+ double cgp(int*arr){
+     double tot=0;
+     for(int i=0;i<5;i++){
+         tot=tot+arr[i];}
+         cgpa=tot/50;
+     return cgpa;
+ }
+ void display(){
+     cout<<name<<endl;
+     cout<<roll<<endl;
+     cout<<tot(marks)<<endl;
+     cout<<cgp(marks);
+ }
+     
+ };
+ int main(){
+     student s;
+     s.stud_inp();
+     s.display();
+     
+ }
